3bc97ffddeecd5098be983f2d77870ff321af6df
India Evans <indiaaeevans@gmail.com>
2017-05-11 09:54:06 -0400

    cleaned up indentation

--- bamazon.sql
+++ bamazon.sql
@@ -13,16 +13,16 @@ CREATE TABLE products (
 	PRIMARY KEY (item_id)
 );
 
--- Populate this database with around 10 different products.
+-- Populate this database a few different products
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES 
-("gobstopper", "candy", 2.25, 100),
-("lollipop", "candy", .87, 2000),
-("peanut butter cup", "candy", 3.00, 20),
-("bouquet of roses", "gift shop", 45.00, 10),
-("swag watch", "gift shop", 200, 28),
-("ipod", "gift shop", 120, 80),
-("case of beer", "office supplies", 10, 100),
-("gel pens", "office supplies", 3, 40),
-("keyboard", "office supplies", 5, 2000),
-("air freshener", "office supplies", 2, 39);
+	("gobstopper", "candy", 2.25, 100),
+	("lollipop", "candy", .87, 2000),
+	("peanut butter cup", "candy", 3.00, 20),
+	("bouquet of roses", "gift shop", 45.00, 10),
+	("swag watch", "gift shop", 200, 28),
+	("ipod", "gift shop", 120, 80),
+	("case of beer", "office supplies", 10, 100),
+	("gel pens", "office supplies", 3, 40),
+	("keyboard", "office supplies", 5, 2000),
+	("air freshener", "office supplies", 2, 39);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,8 @@ var connection = mysql.createConnection({
   database: "Bamazon"
 });
 
+// establish connection to the database--if successful, 
+// run our first function to show the items available and start an order
 connection.connect(function(err) {
   if (err) throw err;
   displayItems();
@@ -17,50 +19,51 @@ connection.connect(function(err) {
 // Display all of the items available for sale
 function displayItems() {
 	var query = "SELECT item_id, product_name, price, stock_quantity FROM products";
-    connection.query(query, function(err, results) {
-    	if (err) throw err;
-    	for (var i = 0; i < results.length; i++) {
+  connection.query(query, function(err, results) {
+    if (err) throw err;
+    for (var i = 0; i < results.length; i++) {
 			console.log("Item ID: " + results[i].item_id + " || Name: " + results[i].product_name + " || Price: $" + results[i].price);
       console.log("______________________________________________________");
 			// console.log("Units in stock: " + results[i].stock_quantity);
-      	}
-      	newOrder();
-     });
+    }
+    newOrder();
+  });
 }
 
 // Prompt user with two messages about what they want to buy
 function newOrder() {
 	inquirer.prompt([
-      { name: "id",
-        type: "input",
-        message: "Please enter the ID of the product you would like to purchase:"
-      },
-      { name: "quantity",
-      	type: "input",
-      	message: "Please specify how many units you would like to buy:"
-      }
-      ]).then(function (answers) {
-      	// check if valid numbers were submitted by the user
-      	if(isNaN(answers.id) || isNaN(answers.quantity)){
-      		console.log("Please enter a valid ID and quantity.")
-      		askUser();
-      	} else {
-      		// confirm what they wanted to order
-      		console.log("You requested " + answers.quantity + " units of item ID " + answers.id + ".");
-      		// check if your store has enough of the product to meet the customer's request.
+    { name: "id",
+      type: "input",
+      message: "Please enter the ID of the product you would like to purchase:"
+    },
+    { name: "quantity",
+      type: "input",
+      message: "Please specify how many units you would like to buy:"
+    }
+  ]).then(function (answers) {
+  // check if valid numbers were submitted by the user
+    if(isNaN(answers.id) || isNaN(answers.quantity)){
+      console.log("Please enter a valid ID and quantity.")
+      askUser();
+    } else {
+    // confirm what they wanted to order
+      console.log("You requested " + answers.quantity + " units of item ID " + answers.id + ".");
+      // check if your store has enough of the product to meet the customer's request.
 			var query = "SELECT stock_quantity, price, product_name FROM products WHERE ?";
-		    connection.query(query, { item_id: answers.id }, function(err, res) {
-		    	if (err) throw err;
-		    	else if (answers.quantity > res[0].stock_quantity){
-		      	console.log("There is not enough in stock to fulfill the order.");
-		      	orderAgain();
-		      	// if your store has enough of the product, fulfill the customer's order.
-		      	} else {
-			      	console.log("There is enough in stock to fulfill the order.");
-			      	// Update the SQL database to reflect the remaining quantity.
-			      	connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [res[0].stock_quantity - answers.quantity, answers.id], 
-			      	function (error, results, fields) {
-						if (error) throw error;
+		  connection.query(query, { item_id: answers.id }, function(err, res) {
+		    if (err) throw err;
+		    else if (answers.quantity > res[0].stock_quantity){
+		      console.log("There is not enough in stock to fulfill the order.");
+		      orderAgain();
+		      // if your store has enough of the product, fulfill the customer's order.
+		    } else {
+          console.log("There is enough in stock to fulfill the order.");
+			    // Update the SQL database to reflect the remaining quantity.
+          var queryUpdate = 'UPDATE products SET stock_quantity = ? WHERE item_id = ?';
+          connection.query(queryUpdate, [res[0].stock_quantity - answers.quantity, answers.id], 
+			    function (error, results, fields) {
+            if (error) throw error;
 				  	// console.log("There are " + res[0].stock_quantity + " units remaining.");
 					});
 					// Once the update goes through, show the customer the total cost of their purchase.
@@ -69,22 +72,24 @@ function newOrder() {
 					console.log("Total cost: $" + totalcost);
 					console.log("Your order was placed successfully!")
 					orderAgain();
-			    }
-		    });
-      	}
-    });
+			  }
+		  });
+    }
+  });
 }
 
-function orderAgain(){
-	inquirer.prompt([{ 
-		name: "order",
-        type: "confirm",
-        message: "Would you like to place a new order?"
-    }]).then(function (answers) {
-    	if (answers.order){
-      		displayItems();
-      	} else {
-      		console.log("Thanks for shopping with Bamazon.  Come back soon!");
-      	}
-    });
+// ask if they would like to start a new order--if yes, then prompt them with the items again
+function orderAgain() {
+	inquirer.prompt([
+    { name: "order",
+      type: "confirm",
+      message: "Would you like to place a new order?"
+    }
+  ]).then(function (answers) {
+    if (answers.order){
+      displayItems();
+    } else {
+      console.log("Thanks for shopping with Bamazon.  Come back soon!");
+    }
+  });
 }
